79801b75202e9968a5531b82f5988c92ca4dbe5f
Gabriel Montes <gabriel@bloq.com>
2021-03-04 18:24:57 -0300

    Handle claim errors

--- packages/pure-finance-lib/src/merkle-claims.js
+++ packages/pure-finance-lib/src/merkle-claims.js
@@ -4,6 +4,8 @@ const createErc20 = require('erc-20-lib')
 const createMerkleBox = require('merkle-box-lib')
 const fetch = require('node-fetch')
 
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
 const parseMemoString = function (memo) {
   return memo
     .split(';')
@@ -30,20 +32,29 @@ const createMerkleClaims = function (web3, options) {
   const getHolding = function (claimGroupId) {
     return merkleBox
       .getHolding(claimGroupId)
-      .then(function ({ erc20, memo }) {
+      .then(function ({ erc20, memo, owner }) {
+        if (owner === ZERO_ADDRESS) {
+          throw new Error('Invalid claim group ID')
+        }
+        const uri = parseMemoString(memo).datasetUri
+        if (!uri) {
+          throw new Error('Could not get extra claim data location')
+        }
         return Promise.all([
           createErc20({ web3, token: erc20 }).getInfo(),
-          getClaimData(parseMemoString(memo).datasetUri, from)
+          getClaimData(uri, from)
         ])
       })
-      .then(function ([token, { amount, proof } = {}]) {
+      .then(function ([token, claimData]) {
+        if (!claimData) {
+          throw new Error('Could not get extra claim data')
+        }
+        const { amount, proof } = claimData
         return Promise.all([
           token,
           amount,
           proof,
-          amount && proof
-            ? merkleBox.isClaimable(claimGroupId, amount, proof)
-            : false
+          merkleBox.isClaimable(claimGroupId, from, amount, proof)
         ])
       })
       .then(function ([token, amount, proof, isClaimable]) {
@@ -56,7 +67,22 @@ const createMerkleClaims = function (web3, options) {
       })
   }
 
-  const claim = merkleBox.claim
+  const claim = function (claimGroupId, amount, proof) {
+    return merkleBox
+      .getHolding(claimGroupId)
+      .then(function ({ owner }) {
+        if (owner === ZERO_ADDRESS) {
+          throw new Error('Invalid claim group ID')
+        }
+        return merkleBox.isClaimable(claimGroupId, from, amount, proof)
+      })
+      .then(function (claimable) {
+        if (!claimable) {
+          throw new Error('Invalid claim or already claimed')
+        }
+        return merkleBox.claim(claimGroupId, from, amount, proof)
+      })
+  }
 
   return {
     getHolding,
