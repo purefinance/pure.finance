81c0100951c8436bd37b064d1da852134ac25067
Gabriel Montes <gabriel@bloq.com>
2021-03-12 11:54:11 -0300

    Add GA tag to collect anonymous usage data

--- site/components/Head.js
+++ site/components/Head.js
@@ -2,6 +2,20 @@ import Head from 'next/head'
 
 const CustomHead = ({ title }) => (
   <Head>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+    ></script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+        `
+      }}
+    />
     <title>Pure Finance {title && ` | ${title}`}</title>
     <link rel="icon" href="/favicon.ico" />
   </Head>
