5bb898387d6fcc5664608a8fc3422ab7557dd537
Gabriel Montes <gabriel@bloq.com>
2021-03-18 17:03:59 -0300

    Improve the token approvals app

--- site/pages/token-approvals.js
+++ site/pages/token-approvals.js
@@ -2,7 +2,6 @@ import { isAddress } from 'web3-utils'
 import { useContext, useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { util } from 'erc-20-lib'
-// import debounce from 'lodash.debounce'
 import vesperMetadata from 'vesper-metadata'
 
 import Button from '../components/Button'
@@ -12,18 +11,24 @@ import PureContext from '../components/context/Pure'
 
 import { fromUnit, toUnit } from '../utils'
 
-// TODO add symbol to Vesper pools and fix name
 const extraTokens = [].concat(
   vesperMetadata.pools.map((p) => ({ ...p, symbol: p.name })),
   vesperMetadata.tokens
 )
 
 const useTokenInput = function (onChange) {
+  const { active, chainId } = useWeb3React()
   const { erc20 } = useContext(PureContext)
 
   const [tokenAddress, setTokenAddress] = useState()
   const [tokenError, setTokenError] = useState()
   const [tokenName, setTokenName] = useState()
+
+  useEffect(() => {
+    setTokenAddress('')
+    setTokenName('')
+    setTokenError('')
+  }, [active, chainId])
 
   const handleChange = function (e) {
     const { value } = e.target
@@ -41,6 +46,7 @@ const useTokenInput = function (onChange) {
       return
     }
 
+    // TODO debounce this call
     const contract = erc20(address)
     contract
       .getInfo()
@@ -57,140 +63,165 @@ const useTokenInput = function (onChange) {
   return {
     caption: tokenError || tokenName,
     captionColor: tokenError && 'text-red-600',
-    disabled: !erc20,
+    disabled: !active,
     onChange: handleChange,
     value: tokenAddress
   }
 }
 
-const useAllowanceInput = function (tokenInfo, spenderInfo, value, onChange) {
-  const { account, erc20 } = useContext(PureContext)
+const useAllowanceInput = function (token, spender, allowance, setAllowance) {
+  const { account, active, chainId } = useWeb3React()
+  const { erc20 } = useContext(PureContext)
 
-  const disabled = !tokenInfo || !spenderInfo || !erc20 || !account
+  const disabled = !token || !spender || !active || !erc20
+
+  useEffect(
+    function () {
+      setAllowance('')
+    },
+    [active, token, spender]
+  )
 
   useEffect(
     function () {
       if (disabled) {
-        onChange()
         return
       }
 
-      const contract = erc20(tokenInfo.address)
+      const contract = erc20(token.address)
       contract
-        .allowance(account, spenderInfo.address)
+        .allowance(account, spender.address)
         .then(function (currentAllowance) {
-          onChange(fromUnit(currentAllowance, tokenInfo.decimals))
+          setAllowance(fromUnit(currentAllowance, token.decimals))
         })
         .catch(function (err) {
-          // TODO send message to form feedback
+          // Send message to feedback too?
           console.warn(err.message)
         })
     },
-    [tokenInfo, spenderInfo]
+    [token, spender, account, chainId]
   )
 
   const handleChange = function (e) {
-    onChange(e.target.value)
+    const re = /^(([1-9][0-9]*)?[0-9](\.[0-9]*)?|\.[0-9]+)$/ // Decimal number
+    if (e.target.value === '' || re.test(e.target.value)) {
+      setAllowance(e.target.value)
+    }
   }
 
   return {
     disabled,
     onChange: handleChange,
-    suffix: tokenInfo && tokenInfo.symbol,
-    value
+    suffix: token && token.symbol,
+    value: allowance
   }
 }
 
-const AllowanceInput = function (props) {
-  const { disabled } = props
+const useFeedback = function () {
+  const { account, active } = useWeb3React()
 
-  const [isInfinite, setInfinite] = useState(false)
+  const [feedback, _setFeedback] = useState({ message: '' })
 
-  const handleInfiniteClick = function () {
-    setInfinite(!isInfinite)
+  useEffect(
+    function () {
+      _setFeedback({ message: '' })
+    },
+    [account, active]
+  )
+
+  const setFeedback = function (type, message) {
+    const colors = {
+      error: 'text-red-600',
+      success: 'text-green-600'
+    }
+    const color = colors[type] || 'text-black'
+    _setFeedback({ color, message })
   }
 
-  // TODO use checkbox value
-
-  return (
-    <>
-      <Input {...props} />
-      <div className="flex items-center justify-center w-full">
-        <label className="space-x-2">
-          <input
-            className="w-4 h-4 border-2"
-            disabled={disabled}
-            onChange={handleInfiniteClick}
-            type="checkbox"
-            value={isInfinite}
-          />
-          <span>Infinite</span>
-        </label>
-      </div>
-    </>
-  )
+  return [feedback, setFeedback]
 }
 
-const TokenApprovalsForm = function () {
-  const [tokenInfo, setTokenInfo] = useState()
-  const tokenInput = useTokenInput(setTokenInfo)
-
-  const [spenderInfo, setSpenderInfo] = useState()
-  const spenderInput = useTokenInput(setSpenderInfo)
-
-  const [allowance, setAllowance] = useState()
-  const allowanceInput = useAllowanceInput(
-    tokenInfo,
-    spenderInfo,
-    allowance,
-    setAllowance
-  )
-
-  const [feedback, setFeedback] = useState({ message: '' })
-  const clearFeedback = () => setFeedback({ message: '' })
-  const setErrorMessage = (message) =>
-    setFeedback({ color: 'text-red-600', message })
-  const setInfoMessage = (message) =>
-    setFeedback({ color: 'text-black', message })
-  const setSuccessMessage = (message) =>
-    setFeedback({ color: 'text-green-600', message })
-
+const useFormButton = function (disabled, setFeedback, onClick) {
   const [inProgress, setInProgress] = useState(false)
-  const approveDisabled = !allowance || inProgress
-  const { erc20 } = useContext(PureContext)
-  const handleApproveClick = function () {
+
+  const handleClick = function () {
     setInProgress(true)
-    setInfoMessage('Approval in Progress')
-    const contract = erc20(tokenInfo.address)
-    // TODO check current allowance first
-    Promise.resolve()
-      .then(() =>
-        contract.approve(
-          spenderInfo.address,
-          toUnit(allowance, tokenInfo.decimals)
-        )
-      )
-      .then(function (receipt) {
-        console.log('DONE', receipt.events.Approval)
-        setSuccessMessage('Approval Succeeded')
+    setFeedback('info', 'Approval in Progress')
+    onClick()
+      .then(function () {
+        setFeedback('success', 'Approval Succeeded')
       })
       .catch(function (err) {
-        setErrorMessage(err.message)
+        setFeedback('error', err.message)
       })
       .finally(function () {
         setInProgress(false)
       })
   }
 
-  const { active, account } = useWeb3React()
-  useEffect(clearFeedback, [active, account, tokenInfo, spenderInfo])
+  return {
+    disabled: disabled || inProgress,
+    onClick: handleClick
+  }
+}
+
+const TokenApprovalsForm = function () {
+  const [token, setToken] = useState()
+  const tokenInput = useTokenInput(setToken)
+
+  const [spender, setSpender] = useState()
+  const spenderInput = useTokenInput(setSpender)
+
+  const [allowance, setAllowance] = useState()
+  const allowanceInput = useAllowanceInput(
+    token,
+    spender,
+    allowance,
+    setAllowance
+  )
+
+  const [feedback, setFeedback] = useFeedback()
+
+  const { account, erc20 } = useContext(PureContext)
+  const approveDisabled = !allowance || !erc20
+  const approveAmount = function (amount) {
+    const contract = erc20(token.address)
+    return contract
+      .allowance(account, spender.address)
+      .then(function (currentAllowance) {
+        if (amount === currentAllowance) {
+          throw new Error('Allowance already set')
+        }
+      })
+      .then(() => contract.approve(spender.address, amount))
+      .then(function (receipt) {
+        console.log('DONE', receipt.events.Approval.returnValues)
+        return contract.allowance(account, spender.address)
+      })
+      .then(function (newAllowance) {
+        setAllowance(fromUnit(newAllowance, token.decimals))
+      })
+  }
+  const approveAllowance = () =>
+    approveAmount(toUnit(allowance, token.decimals))
+  const approveInfinite = () => approveAmount((2n ** 256n - 1n).toString()) // 2 ^ 256 - 1
+  const approveButton = useFormButton(
+    approveDisabled,
+    setFeedback,
+    approveAllowance
+  )
+  const infiniteButton = useFormButton(
+    approveDisabled,
+    setFeedback,
+    approveInfinite
+  )
 
   return (
     <>
       <div className="flex flex-wrap justify-center w-full max-w-lg mx-auto mt-10 space-y-4">
         <div className="w-full h-24">
           <Input
-            placeholder="symbol or address"
+            placeholder="address or symbol"
             title="Token Address:"
             {...tokenInput}
           />
@@ -202,16 +233,17 @@ const TokenApprovalsForm = function () {
             {...spenderInput}
           />
         </div>
-        <AllowanceInput
-          suffix={tokenInfo && tokenInfo.symbol}
+        <Input
+          suffix={token && token.symbol}
           title="Allowance:"
           {...allowanceInput}
         />
       </div>
       <div className="flex justify-center mt-7.5">
-        <Button disabled={approveDisabled} onClick={handleApproveClick}>
-          APPROVE
-        </Button>
+        <Button {...approveButton}>APPROVE ALLOWANCE</Button>
+      </div>
+      <div className="flex justify-center mt-7.5">
+        <Button {...infiniteButton}>APPROVE INFINITE</Button>
       </div>
       <p className={`text-center text-sm mt-6 ${feedback.color}`}>
         {feedback.message}
