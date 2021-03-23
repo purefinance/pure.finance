5b3d52881d0585eb292c936f3de3655adf3917be
Gabriel Montes <gabriel@bloq.com>
2021-03-23 20:44:29 -0300

    Improve parsing of errors coming from WC

--- packages/pure-finance-lib/lib/parse-evm-error.js
+++ packages/pure-finance-lib/lib/parse-evm-error.js
@@ -6,7 +6,8 @@ const tryParseEvmError = function (err) {
   const matches =
     err.message.match(/: revert (.*)"/) ||
     err.message.match(/"reason": "(.*)"/) ||
-    err.message.match(/reverted: (.*)"/)
+    err.message.match(/reverted: (.*)"/) ||
+    err.message.match(/reverted: (.*)/)
   if (matches) {
     throw new Error(capitalize(matches[1]))
   }
