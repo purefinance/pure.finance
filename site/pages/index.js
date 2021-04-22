8709f527dc9db363548110ae830816d45802c79c
Martin Bon Foster <martin.bonfoster@bloq.com>
2021-04-22 20:23:01 -0300

    Fix site name on deploy script and modify site to deploy

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,14 +1,12 @@
 import Layout from '../components/Layout'
 import Utilities from '../components/Utilities'
 
-function HomePage() {
-  return (
-    <Layout>
-      <div className="w-full mb-14">
-        <Utilities />
-      </div>
-    </Layout>
-  )
-}
+const HomePage = () => (
+  <Layout>
+    <div className="w-full mb-14">
+      <Utilities />
+    </div>
+  </Layout>
+)
 
 export default HomePage
