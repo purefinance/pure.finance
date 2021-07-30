3d13c625a97b489d0c03f8f12343ecf3debff33f
Gabriel Montes <gabriel@bloq.com>
2021-07-30 19:32:29 -0300

    Update footer link an spacing

--- site/components/Footer.js
+++ site/components/Footer.js
@@ -1,23 +1,20 @@
-import Link from 'next/link'
-import Vesper from './svg/Vesper'
 import LanguageSelector from './LanguageSelector'
+import Vesper from './svg/Vesper'
 import useTranslation from 'next-translate/useTranslation'
 
 const Footer = function () {
   const { t } = useTranslation('common')
   return (
-    <div className="flex flex-wrap justify-center w-full">
+    <div className="flex flex-wrap justify-center w-full mt-20">
       <div className="w-full">
         <p className="text-xs font-semibold text-center opacity-75 text-vesper">
           {t('sponsored-by').toUpperCase()}
         </p>
       </div>
       <div className="mt-2">
-        <Link href="https://vesper.finance">
-          <a rel="noreferrer" target="_blank">
-            <Vesper />
-          </a>
-        </Link>
+        <a href="https://vesper.finance" rel="noreferrer" target="_blank">
+          <Vesper />
+        </a>
       </div>
       <div className="flex justify-between w-full pt-3 mt-20 text-xs text-gray-500 border-t-2 space-x-4">
         <div className="w-1/2">
