697b28afa34631c2d8676616d835ef43f517c96a
Thomas Alvarenga <thomas@bloq.com>
2024-09-17 13:06:12 -0300

    Added translations for hero title

--- site/components/layout/Hero.js
+++ site/components/layout/Hero.js
@@ -1,19 +1,25 @@
+import { useTranslations } from 'next-intl'
+
 import { HemiLogoDashed } from '../HemiLogo'
 
-const Hero = () => (
-  <div className="border-orange-950 from-orange-opacity relative flex flex-col items-center justify-center -mt-24 pb-20 px-8 bg-gradient-to-t border-b overflow-hidden">
-    <div className="relative flex justify-center">
-      <div className="w-128 absolute">
-        <HemiLogoDashed />
+const Hero = () => {
+  const t = useTranslations()
+  return (
+    <div className="border-orange-950 from-orange-opacity relative flex flex-col items-center justify-center -mt-24 pb-20 px-8 bg-gradient-to-t border-b overflow-hidden">
+      <div className="relative flex justify-center">
+        <div className="w-128 absolute">
+          <HemiLogoDashed />
+        </div>
+      </div>
+      <div className="z-10 mt-64 md:mt-72">
+        <h1 className="max-w-xl text-center text-4xl font-semibold md:max-w-3xl md:text-5xl">
+          {t('empower-journey')}{' '}
+          <span className="text-orange-950">{t('cutting-edge')}</span>{' '}
+          {t('tools')}
+        </h1>
       </div>
     </div>
-    <div className="z-10 mt-64 md:mt-72">
-      <h1 className="max-w-xl text-center text-4xl font-semibold md:max-w-3xl md:text-5xl">
-        Empower Your Web3 Journey with{' '}
-        <span className="text-orange-950">Cutting-Edge</span> Tools
-      </h1>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Hero
