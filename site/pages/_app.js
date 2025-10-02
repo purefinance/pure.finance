eb78f659784b191a6f3cdca8c363db735f854a7e
Gabriel Montes <gabriel@hemi.xyz>
2025-10-02 12:21:40 -0400

    Patch web3 provider to stop offering site fees

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -8,13 +8,31 @@ import Web3 from 'web3'
 import { PureContextProvider } from '../components/context/Pure'
 import { defaultLocale } from '../navigation'
 
-const getLibrary = provider => new Web3(provider)
-
 // Force Big to round down.
 // See https://github.com/MikeMcl/big.js/blob/v5.2.2/big.js#L26
 Big.RM = 0
 
-const App = ({ Component, pageProps }) => {
+function getLibrary(provider) {
+  if (provider.isMetaMask) {
+    // To force MetaMask to use market suggested fees, we must intercept the
+    // calls to request() with method eth_sendTransaction and clear out all
+    // gas-related properties. ¯\_(ツ)_/¯
+    // Note: Tested with v1.10.4. Newer versions of web3 may break this patch.
+    const request = provider.request.bind(provider)
+    const wrappedRequest = function ({ method, params }) {
+      if (method === 'eth_sendTransaction') {
+        params[0].gasPrice = undefined
+        params[0].maxFeePerGas = undefined
+        params[0].maxPriorityFeePerGas = undefined
+      }
+      return request({ method, params })
+    }
+    provider.request = wrappedRequest
+  }
+  return new Web3(provider)
+}
+
+function App({ Component, pageProps }) {
   const router = useRouter()
   return (
     <NextIntlClientProvider
