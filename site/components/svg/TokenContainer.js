83cad447d09e64ab3195d153358d6525ac738cff
Gabriel Montes <gabriel@hemi.xyz>
2025-10-13 16:01:22 -0400

    Update token lists and icon container

--- site/components/svg/TokenContainer.js
+++ site/components/svg/TokenContainer.js
@@ -1,15 +1,34 @@
-import svgList from './tokens/'
+import Image from 'next/image'
 
-const TokenContainer = function ({ name, ...props }) {
+import Ether from './tokens/Ether'
+
+const { findTokenBySymbol } = require('token-list')
+
+const TokenContainer = function ({ chainId = 1, name, ...props }) {
   if (!name) {
     return null
   }
-  const SvgComponent = svgList[name.toLowerCase()]
-  if (!SvgComponent) {
-    console.warn(`SVG component missing: ${name}`)
-    return null
+
+  if (name === 'ETH') {
+    return <Ether {...props} />
   }
-  return <SvgComponent {...props} />
+
+  const token = findTokenBySymbol(name, chainId)
+  if (token && token.logoURI) {
+    return (
+      <Image
+        alt={token.symbol}
+        className="rounded-full"
+        height="20"
+        src={token.logoURI}
+        width="20"
+        {...props}
+      />
+    )
+  }
+
+  console.warn(`Token icon is missing for ${name}`)
+  return null
 }
 
 export default TokenContainer
